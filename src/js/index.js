e07888a8eefd4add5650c9ecb86b8e7477d0af31
liuyang <place222@hotmail.com>
2019-03-12 08:45:41 +0800

    done

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,16 @@
 import '../css/reset.css'
 import '../css/common.css'
 import '../css/app.css'
+import $ from 'jquery'
 
-
-window.onload = function () {
+$(function(){
     var cards = document.querySelectorAll('.mycode')
     document.addEventListener('scroll', function () {
         checkScroll(cards)
     })
     checkScroll(cards)
-}
+})
+
 
 
 function checkScroll(cards){
